a9f073aec1b41d188a9db4751195f3eb3b9dbf6a
Ulises232 <ldaulises666@gmail.com>
2022-09-22 17:46:36 -0500

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ejemploAngular';
+  pelicula={
+    titulo : 'Spiderman',
+    fechaLanzamiento : new Date,
+    precio : 235.76
+  }
+  duplicaValor(x : number ):number{
+    return x * 2;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SumaComponent } from './calculos/suma/suma.component';
+import { RestaComponent } from './calculos/resta/resta.component';
+
+import { FormsModule } from '@angular/forms';
+import { ProductListComponent } from './podruct/product-list/product-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SumaComponent, RestaComponent, ProductListComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
